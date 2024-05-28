6ed48313057df95f59688a4dad174c1ec0094b22
Phickayor <jetawof@gmail.com>
2024-05-28 11:24:49 +0100

    Leadership Ui Change

--- components/About/Leadership.tsx
+++ components/About/Leadership.tsx
@@ -2,42 +2,37 @@ import React from "react";
 
 function Leadership() {
   return (
-    <div className="text-center py-10" id="leadership">
-      <h1 className="text-2xl md:text-3xl lg:text-4xl font-satoshi-semibold ">
-        Our Leadership
-      </h1>
-      <p className="mx-auto md:w-10/12 py-4 md:leading-loose md:text-lg text-[#516371] ">
-        Our leadership team at Unique Agility LLC, comprised of seasoned
-        professionals, excels in innovation, client satisfaction, and
-        operational excellence. They guide the company with strategic thinking,
-        business strategy, and technology management, ensuring our services are
-        technically proficient and aligned with client needs. Committed to
-        fostering a collaborative culture, they prioritize talent development
-        for both internal teams and clients, driving continuous improvement.
-        Their expertise and integrity are key to the success of Unique Agility
-        LLC.
-      </p>
-      <div className="flex md:flex-row flex-col gap-3 md:gap-5 w-full py-2">
-        <div className="w-full md:w-2/5 relative self-center py-5 md:py-10 lg:py-0 lg:self-auto">
+    <div className="bg-offWhite py-14" id="leadership">
+      <div className="flex lg:flex-row flex-col justify-between gap-10">
+        <div className="w-full gap-5 flex flex-col">
           <img
-            src="/images/ellipse.png"
-            className="absolute bottom-0 left-0 w-full z-10 inset-0 lg:inset-full "
+            src="/images/ceo-office.jpg"
+            alt=""
+            className="object-cover w-56 h-56 self-center lg:w-full lg:h-72 xl:h-96 rounded-full lg:rounded-2xl"
           />
-          <div className="absolute inset-0 bg-green opacity-80 rounded-2xl"></div>
-          <div className="h-full w-full relative px-5 flex flex-col justify-center gap-4">
-            <label className="text-white">Our Founder/CEO</label>
-            <h1 className="text-white text-2xl sm:text-3xl xl:text-4xl font-satoshi-semibold">
+          <div className="space-y-3 text-center hidden lg:block">
+            <h1 className="font-bold text-primaryHeader text-2xl">
               Onyinye Nwegbe
             </h1>
+            <label className="font-petrona">Our Founder / CEO</label>
           </div>
         </div>
-        <div className="w-full bg-[#F8F8F8] p-5 lg:p-10 rounded-2xl">
-          <p className="xl:text-2xl text-[#7D818C]">
-            Onyinye Nwegbe, Founder and CEO of Unique Agility LLC, is an expert
-            in Agile methodologies with MBAs and certifications like PMP and
-            SAFe RTE. He has held senior roles at major companies, excelling in
-            team coaching and Agile transitions. He leads Unique Agility in
-            innovation, community development, and impactful business solutions.
+        <div className="flex flex-col gap-5 w-full">
+          <h1 className="text-3xl xl:text-4xl font-bold">Our Leadership</h1>
+          <p className="xl:text-lg">
+            Unique Agility LLC's leadership excels in innovation, client
+            satisfaction, and operational excellence, fostering talent and
+            strategic, technically proficient services.
+          </p>
+          <p className="xl:text-lg">
+            <span className="font-satoshi-semibold text-green">
+              Onyinye Nwegbe
+            </span>
+            , Founder and CEO of Unique Agility LLC, is an expert in Agile
+            methodologies with MBAs and certifications like PMP and SAFe RTE. He
+            has held senior roles at major companies, excelling in team coaching
+            and Agile transitions. He leads Unique Agility in innovation,
+            community development, and impactful business solutions.
           </p>
         </div>
       </div>
