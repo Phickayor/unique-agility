4f137d7c09f91c8fd5de43ad42c499c421a5b3e2
Phickayor <jetawof@gmail.com>
2024-06-04 11:34:44 +0100

    form on frontend only

--- components/Contact/ContactForm.tsx
+++ components/Contact/ContactForm.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function ContactForm() {
   return (
-    <form className="text-center space-y-3 sm:space-y-5 ">
+    <form className="text-center space-y-3 sm:space-y-5 " action="mailto:info@uniqueagility.com" method="GET">
       <h1 className="text-2xl md:text-4xl font-satoshi-bold">
         Connect with us
       </h1>
@@ -12,17 +12,16 @@ function ContactForm() {
       </p>
       <div className="grid md:grid-cols-2 mx-auto py-4 lg:w-8/12 gap-5 [&>*]:border-2 [&>*]:rounded-lg md:[&>*]:text-lg [&>*]:p-3 focus:[&>*]:outline-green">
         <input type="text" placeholder="Your Name" />
-        <input type="text" placeholder="Email" />
-        <input type="text" placeholder="Phone Number" />
-        <input type="text" placeholder="Location"/>
+        <input type="text" name="subject" placeholder="Subject" />
         <input
           type="text"
+          name="body"
           placeholder="Your message"
-          className="md:col-span-2"
+          className="md:col-span-2 h-32"
         />
       </div>
 
-      <button className="bg-green font-semibold text-white px-8 py-3 rounded-2xl">
+      <button type="submit" className="bg-green font-semibold text-white px-8 py-3 rounded-2xl">
         Contact us
       </button>
     </form>
