04eb335188d000f02170948227ae9181cab280c8
Phickayor <jetawof@gmail.com>
2024-06-19 12:59:47 +0100

    layout changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,6 +28,9 @@ export default function RootLayout({
       lang="en"
       className={`${inter.variable} ${asap.variable} ${jakar.variable}`}
     >
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </head>
       <body>{children}</body>
     </html>
   );
